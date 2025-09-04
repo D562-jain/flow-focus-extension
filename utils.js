020cfab8f2fee7c357bf2fd30d87274229c2b401
Disha Jain <jain.disha104@gmail.com>
2025-09-04 19:18:06 +0530

    Delete utils.js

--- utils.js
+++
@@ -1,7 +0,0 @@
-// This function opens a long-lived connection to keep the service worker alive.
-// It's a common workaround for Manifest V3 service worker termination.
-(function connect() {
-  chrome.runtime
-    .connect({ name: "keepAlive" })
-    .onDisconnect.addListener(connect);
-})();
